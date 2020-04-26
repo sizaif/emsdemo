ed590012a8a073669fcfb55aa59dca98ecf3ebfd
sizaif <Sizaif2000@gmail.com>
2020-04-26 14:52:05 +0800

    1:(后台) : 1.1 公告栏项目全部完成

--- src/main/java/com/sizaif/emsdemo/controller/AnnounceController.java
+++ src/main/java/com/sizaif/emsdemo/controller/AnnounceController.java
@@ -1,7 +1,10 @@
 package com.sizaif.emsdemo.controller;
 
+import com.sizaif.emsdemo.Result.SystemResult;
 import com.sizaif.emsdemo.pojo.Announce.Announce;
+import com.sizaif.emsdemo.pojo.User.Users;
 import com.sizaif.emsdemo.service.Announce.AnnounceService;
+import com.sizaif.emsdemo.utils.DateUtils;
 import com.sizaif.emsdemo.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -32,8 +36,11 @@ public class AnnounceController {
     private AnnounceService announceService;
 
 
-
-
+    /**
+     * 去公告界面
+     * @param model
+     * @return
+     */
     @RequestMapping("/toManagePage")
     public String toManagePage(Model model){
 
@@ -59,4 +66,120 @@ public class AnnounceController {
         }
         return  null;
     }
+
+    /**
+     * 设置公告是否启用关闭
+     * @param id
+     * @param isEnabled
+     * @return
+     */
+    @RequestMapping("/setEnabled")
+    @ResponseBody
+   public String switchEnabled(@RequestParam("id") Integer id,
+                               @RequestParam("isEnabled") Integer isEnabled){
+
+        logger.debug("设置公告是否启用！id:" + id + ",isEnabled:" + isEnabled);
+        String msg = "";
+        try {
+            if (null == id || null == isEnabled) {
+                logger.debug("请求参数有误，请您稍后再试");
+                return "请求参数有误，请您稍后再试";
+            }
+
+            // 设置用户是否启用
+            Announce announce = new Announce();
+            announce.setId(id);
+            // 是否启用状态
+            announce.setIsEnabled(isEnabled);
+            announce.setModifyDate(DateUtils.DatetoString(new Date()));
+            logger.debug(announce.toString());
+            SystemResult systemResult = announceService.updateSelective(announce);
+            if (systemResult.getStatus() == 200) {
+                logger.debug("更新公告是否启用状态成功");
+                return "200";
+            } else {
+                logger.debug("更新公告是否启用状态失败");
+                return "100";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("设置公告是否启用异常！", e);
+            msg = "操作异常，请您稍后再试！";
+        }
+        return msg;
+    }
+
+
+    @RequestMapping("/addAnnounce")
+    @ResponseBody
+    public String addAnnounce(Announce announce){
+
+
+
+        try {
+            announce.setIsEnabled(0);
+            announce.setCreateDate(DateUtils.DatetoString(new Date()));
+            announce.setModifyDate(DateUtils.DatetoString(new Date()));
+            logger.debug("开始添加 announce " +announce.toString());
+            SystemResult systemResult = announceService.insertSelective(announce);
+            if(systemResult.getStatus()==200){
+                return "200";
+            }else
+                return "100";
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return "100";
+    }
+
+    /**
+     * 更新
+     * @param announce
+     * @return
+     */
+   @RequestMapping("/updateAnnounce")
+   @ResponseBody
+   public String updateAnnounce(Announce announce){
+
+        try {
+            //  这是 修改时间
+            announce.setModifyDate(DateUtils.DatetoString(new Date()));
+            logger.debug("开始 更新 announce" + announce.toString());
+            SystemResult systemResult = announceService.updateSelective(announce);
+            if(systemResult.getStatus()==200){
+                return "200";
+            }else
+                return "100";
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return "100";
+   }
+
+
+    /**
+     * 删除
+     * @param id
+     * @return
+     */
+   @RequestMapping("/delAnnounce")
+   @ResponseBody
+   public String delAnnounce(@RequestParam("id") Integer id) {
+
+        try {
+         logger.debug("开始 删除 某个公告");
+            Announce announce = new Announce();
+            announce.setId(id);
+            SystemResult systemResult = announceService.deleteByPrimaryKey(announce);
+            if(systemResult.getStatus()==200){
+                return "200";
+            }else
+                return "100";
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return "100";
+   }
+
 }
--- src/main/resources/static/src/js/Announce/announce.js
+++ src/main/resources/static/src/js/Announce/announce.js
@@ -1,99 +1,104 @@
 $(function(){
-    layui.use(['layer','form','laydate'], function(){
+    layui.extend({tinymce: '/vendors/tinymce/tinymce'}).use(['tinymce', 'util', 'layer','form','element'], function () {
+        var tinymce = layui.tinymce
         var layer = layui.layer;
         var form  = layui.form;
-        var laydate = layui.laydate;
+        var element = layui.element;
 
-
-
-
+        var edit = tinymce.render({
+            elem: "#editmemo"
+            , height: 600
+            , width:'100%'
+        });
+        //
 
         //监听提交
-        form.on('submit(ContestSumbit)', function(data){
+        form.on('submit(AnnounceSumbit)', function(data){
 
+
+            // 获取编辑器文本内容
+            var inputtext = tinymce.get('#editmemo').getContent({format:'text'});
+            // 获取内容 添加到 memo中
+            $("#memohidden").val(inputtext);
+            // layer.alert(inputtext);
             layer.confirm('确定提交么？', {
                 btn: ['确认','返回'] //按钮
             },function(){
                 // layer.closeAll();//关闭所有弹框
-                console.log("data--"+data);
-                formSubmit(data);
+                formSubmit();
             },function() {
                 layer.closeAll();
             });
             return false;
         });
-
-
         form.render();
+
     });
 });
 
 
 //提交数据
-function formSubmit(obj) {
+function formSubmit() {
 
-    if(checkRole()){
-        if($("#flag").val() == "update"){
-            // console.log($("#UserForm").serialize());
-            $.ajax({
-                type: "POST",
-                data: $("#UserForm").serialize(),
-                url: "/users/updateUMRInfo",
-                success: function (data) {
-                    if (data == "200") {
-                        layer.alert("操作成功",function(){
-                            layer.closeAll();
-                            // load();
-                        });
-                    } else {
-                        layer.alert("SSSS");
-                    }
-                },
-                error: function (data) {
-                    layer.alert("操作请求错误，请您联系技术人员");
+    console.log($("#AnnounceForm").serialize(),);
+    if($("#flag").val() == "update"){
+        // console.log($("#UserForm").serialize());
+        $.ajax({
+            type: "POST",
+            data: $("#AnnounceForm").serialize(),
+            url: "/admin/announce/updateAnnounce",
+            success: function (data) {
+                if (data == "200") {
+                    layer.alert("操作成功",function(){
+                        parent.location.reload();
+                        // load();
+                    });
+                } else {
+                    layer.alert("操作失败" + data);
+                    parent.location.reload();
                 }
-            });
-        }else if($("#flag").val() == "add"){
-            console.log($("#UserForm").serialize());
-            $.ajax({
-                type: "POST",
-                data: $("#UserForm").serialize(),
-                url: "/users/addUMRInfo",
-                success: function (data) {
-                    console.log(data);
-                    if (data == "200") {
-                        layer.alert("添加成功!", function () {
-                            layer.closeAll();
-                        });
-                    } else if(data == "100"){
-                        layer.alert("账户已存在,请重试", function () {
-                            layer.closeAll();
-                        });
-                    }
-                    else if(data == "101"){
-                        layer.alert("邮箱已存在,请重试", function () {
-                            layer.closeAll();
-                        });
-                    }
-                },
-                error: function (data) {
-                    layer.alert("操作请求错误，请您联系技术人员");
+            },
+            error: function (data) {
+                layer.alert("操作请求错误，请您联系技术人员");
+            }
+        });
+    }else if($("#flag").val() == "add"){
+        // console.log($("#UserForm").serialize());
+        $.ajax({
+            type: "POST",
+            data: $("#AnnounceForm").serialize(),
+            url: "/admin/announce/addAnnounce",
+            success: function (data) {
+                console.log(data);
+                if (data == "200") {
+                    layer.alert("添加成功!", function () {
+                        parent.location.reload();
+                    });
+                } else {
+                    layer.alert("添加失败", function () {
+                        parent.location.reload();
+                    });
                 }
-            });
-        }
+
+            },
+            error: function (data) {
+                layer.alert("操作请求错误，请您联系技术人员");
+            }
+        });
     }
 
 }
 // 切换是否启用
-function switchEnable(id,name,flag) {
+function switchEnable(id,flag) {
 
     var isEnabled = flag;
     console.log(isEnabled);
     var state = isEnabled == true ? "启用":"未启用";
-    layer.confirm('您确定要把用户：<font style=\'font-weight:bold;\' color=\'blue\'>'+name+'</font>设置为 <font style=\'font-weight:bold;\' color=\'red\'>'+state+'</font> 状态吗？', {
+    var Enabled = isEnabled == true ? 1:0;
+    layer.confirm('您确定要把公告：<font style=\'font-weight:bold;\' color=\'blue\'>'+id+'</font>设置为 <font style=\'font-weight:bold;\' color=\'red\'>'+state+'</font> 状态吗？', {
         btn: ['确认','返回'] //按钮
     }, function(){
-        $.post("/users/setEnabled",{"id":id,"isEnabled":isEnabled},function(data){
+        $.post("/admin/announce/setEnabled",{"id":id,"isEnabled":Enabled},function(data){
 
             if(data=="200"){
                 //回调弹框
@@ -123,158 +128,46 @@ function switchEnable(id,name,flag) {
 function edit(obj) {
 
     console.log(obj);
-    var isLocked = obj.users.locked;
-    if(isLocked){
-        layer.msg("该用户已经被锁定，不可进行编辑；</br>  如需编辑，请设置为<font style='font-weight:bold;' color='green'>未锁定</font>状态。", {icon: 2});
-    }else{
+    $("#flag").val("update");
+    $("#id").val(obj.id == null?'':obj.id);
+    $("#creatorId").val(obj.creatorId == null?'':obj.creatorId);
+    $("#creatorName").val(obj.creatorName == null?'':obj.creatorName);
 
-        $("#id2").val(obj.users.id==null?'':obj.users.id);
-        $("#id").val(obj.users.id==null?'':obj.users.id);
-        $("#truename").val(obj.truename==null?'':obj.truename);
-        $("#school").val(obj.school==null?'':obj.school);
-        $("#mobile").val(obj.users.mobile==null?'':obj.users.mobile);
-        $("#email").val(obj.email==null?'':obj.email);
-        $("#email2").val(obj.email==null?'':obj.email);
-        $("#telephone").val(obj.phone==null?'':obj.phone);
-        $("#address").val(obj.address==null?'':obj.address);
-        $("#username").val(obj.users.name==null?'':obj.users.name);
 
-        $("#flag").val("update");
-
-        // start 初始化 全部角色信息 全部角色信息 显示角色数据
-        var existRole='';
-        if(obj.users.userRoles !=null ){
-            existRole+=obj.users.userRoles.roleId+',';
-            // console.log("item.roleid->"+existRole);
-
-        }
-        // console.log("existRole->" +existRole);
-        $("#roleDiv").empty();
-        $.get("/auth/getRoles",function(data){
-            // console.log(data);
-
-            var jsonobj= eval('(' + data + ')');
-            $.each(jsonobj,function (index, item) {
-
-                var div=$("<option  name='roleId' value="+item.id+" title="+item.descpt+">"+
-                    "<span>"+item.descpt+"</span>"+
-                    "</option>");
-                if(existRole!='' && existRole.indexOf(item.id)>=0){
-
-                    div=$("<option   name='roleId' value="+item.id+" title="+item.descpt+" selected >"+
-                        "<span>"+item.descpt+"</span>"+
-                        "</option>");
-                }
-                $("#roleDiv").append(div);
-            });
-
-            // start 初始化 性别
-            if(obj.gender == 0){
-                // console.log(obj.gender);
-                $("#men").removeAttr("checked");
-                $("#women").attr("checked","checked");
-            }
-            layui.form.render('radio');
-            // end 初始化 性别
-
-            layui.form.render('select');
+    layui.extend({tinymce: '/vendors/tinymce/tinymce'}).use(['tinymce', 'util', 'layer'], function () {
+        var tinymce = layui.tinymce
+        tinymce.render({
+            elem: "#editmemo"
+            , height: 600
+            , width: '100%'
         });
-        // end 初始化 全部角色信息
+        tinymce.get('#editmemo').setContent(obj.memo);
+    });
 
-        // start  初始化 出生日期 弹窗
-        layui.use(['laydate','layer'], function(){
-            var laydate = layui.laydate;
-            var layer =layui.layer;
-            laydate.render({
-                elem: '#birth',
-                type: 'datetime',
-                value:obj.birth
-            });
-            layer.open({
-                type:1,
-                title: "编辑用户详细信息",
-                fixed:false,
-                resize :false,
-                offset:'auto',
-                shadeClose: true,
-                btn: '关闭',
-                area: ['600px', '720px'],
-                content:$('#eidtUser'),
-                end: function(){
-                    location.reload();
-                }
-            });
-        });
-        // end  初始化 出生日期 弹窗
+    $("#showAccordion").addClass("layui-show")
 
-    }
 }
 
 // 添加
-function add(obj) {
-
+function add(uid,uname) {
     $("#flag").val("add");
-    $("#id").remove();
-    var existRole='';
-    // start 全部角色信息 显示角色数据
-    $("#roleDiv").empty();
-
-    $.get("/auth/getRoles",function(data){
-        console.log(data);
-
-        var jsonobj= eval('(' + data + ')');
-        $.each(jsonobj,function (index, item) {
-
-            var div=$("<option  name='roleId' value="+item.id+" title="+item.descpt+">"+
-                "<span>"+item.descpt+"</span>"+
-                "</option>");
-            if(existRole!='' && existRole.indexOf(item.id)>=0){
-
-                div=$("<option   name='roleId' value="+item.id+" title="+item.descpt+" selected >"+
-                    "<span>"+item.descpt+"</span>"+
-                    "</option>");
-            }
-            $("#roleDiv").append(div);
-        });
-        layui.form.render('select');
-    });
-    // end 全部角色信息 显示角色数据
-
     // start 初始化出生日期 和弹窗
-    layui.use(['laydate','layer'], function(){
-        var laydate = layui.laydate;
-        var layer =layui.layer;
-        laydate.render({
-            elem: '#birth'
-            ,type: 'datetime'
-        });
-        layer.open({
-            type:1,
-            title: "添加一个用户以及信息",
-            fixed:false,
-            resize :false,
-            offset:'auto',
-            shadeClose: true,
-            btn: '关闭',
-            area: ['600px', '740px'],
-            content:$('#eidtUser'),
-            end: function(){
-                location.reload();
-            }
-        });
-    });
-    // end 初始化出生日期 和弹窗
+    $("#creatorId").val(uid);
+    $("#creatorName").val(uname);
 
+    $("#showAccordion").addClass("layui-show")
+
+    // end 初始化出生日期 和弹窗
 }
 
 // 删除
 
-function del(id,name) {
+function del(id) {
     if(null!=id){
-        layer.confirm('您确定要 <font style=\'font-weight:bold;\' color=\'red\'>删除: </font><font style=\'font-weight:bold;\' color=\'blue\'>'+name+'</font> 用户么?', {
+        layer.confirm('您确定要 <font style=\'font-weight:bold;\' color=\'red\'>删除序号: '+id+' </font> 的公告么?', {
             btn: ['确认','返回'] //按钮
         },  function(){
-            $.get("/users/toDeleteUser",{"id":id},function(data){
+            $.get("/admin/announce/delAnnounce",{"id":id},function(data){
                 console.log(data)
                 if(data == "200"){
                     //回调弹框
